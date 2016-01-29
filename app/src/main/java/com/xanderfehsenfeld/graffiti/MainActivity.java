4dc7279176adf02bda13b081d2e661557051e0b4
Xander <alexander.fehsenfeld@coloradocollege.edu>
2016-01-28 18:01:12 -0700

    Added a toggle switch to control read and write mode Added errors for empty write string and empty read string Added more toast notifications

--- app/src/main/java/com/xanderfehsenfeld/graffiti/MainActivity.java
+++ app/src/main/java/com/xanderfehsenfeld/graffiti/MainActivity.java
@@ -14,7 +14,9 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -31,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     IntentFilter[] intentFiltersArray;
     String[][] techListsArray;
     EditText et;
+    Toast read_toast;
+    Toast write_toast;
+    Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,8 +64,30 @@ public class MainActivity extends AppCompatActivity {
 
         techListsArray = new String[][] { new String[] { NfcF.class.getName() } };
 
+        /* init toasts */
+        read_toast = Toast.makeText(MainActivity.this, getResources().getString(R.string.read_note), Toast.LENGTH_SHORT);
+        write_toast = Toast.makeText(MainActivity.this, getResources().getString(R.string.write_note), Toast.LENGTH_SHORT);
+
+        /* init switch */
+        aSwitch = (Switch)findViewById(R.id.toggler);
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    read_toast.show();
+                } else {
+                    // The toggle is disabled
+                    write_toast.show();
+                }
+                resolveHint();
+            }
+        });
+
         et = (EditText) findViewById( R.id.graffiti);
         resolveHint();
+
+
+
     }
 
     @Override
@@ -75,25 +102,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void resolveHint(){
-        if (readMode) {
+        if (aSwitch.isChecked()) {
             et.setHint(R.string.read_default);
-            et.setClickable(false);
+
         } else {
             et.setHint(R.string.write_hint);
-            et.setClickable(true);
+
         }
     }
 
-    public void toggle(View view){
-        readMode = !readMode;
-        et.setText("");
-        resolveHint();
-    }
+//    public void toggle(View view){
+//        readMode = !readMode;
+//        et.setText("");
+//        resolveHint();
+//    }
 
     public void onNewIntent(Intent intent) {
         /* rebeccas github : rebecca-watson */
+        Toast toast = Toast.makeText(MainActivity.this, getResources().getString(R.string.read_new_graffiti), Toast.LENGTH_SHORT);
 
-        if (readMode) {
+        /* read mode */
+        if (aSwitch.isChecked()) {
             String s = new String();
 
             Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
@@ -117,37 +146,52 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
+            if (s.trim().length()!=0) {
+                EditText et = (EditText) findViewById(R.id.graffiti);
+                et.setText(s);
+            } else {
+                toast.setText(getResources().getString(R.string.read_error_message));
+            }
 
-            EditText et = (EditText) findViewById(R.id.graffiti);
-            et.setText(s);
 
+
+
+        /* write mode */
         } else {
-            Toast toast = Toast.makeText(MainActivity.this, "detected!", Toast.LENGTH_SHORT);
-            toast.show();
+
             EditText et = (EditText) findViewById(R.id.graffiti);
             String s = et.getText().toString().trim();
+            if (s.length() != 0) {
 
-            Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            NdefRecord[] records = new NdefRecord[0];
-            try {
-                records = new NdefRecord[]{ createRecord(s) };
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
+                Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                NdefRecord[] records = new NdefRecord[0];
+                try {
+                    records = new NdefRecord[]{createRecord(s)};
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
 
-            NdefMessage message = new NdefMessage(records);
-            Ndef ndef = Ndef.get(tagFromIntent);
-            try {
-                ndef.connect();
-                ndef.writeNdefMessage(message);
-                ndef.close();
+                NdefMessage message = new NdefMessage(records);
+                Ndef ndef = Ndef.get(tagFromIntent);
+                try {
+                    ndef.connect();
+                    ndef.writeNdefMessage(message);
+                    ndef.close();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (FormatException e) {
-                e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (FormatException e) {
+                    e.printStackTrace();
+                }
+
+                toast.setText(getResources().getString(R.string.wrote_graffiti));
+            } else {
+                toast.setText(getResources().getString(R.string.write_error_message));
             }
         }
+
+        /* show a notification */
+        toast.show();
 
 
     }
